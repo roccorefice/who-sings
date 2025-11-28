09ba25f7e7f466fe3547aa8285b556d932379814
rocco.orefice <rocco.orefice@fiven.eu>
2025-11-28 17:00:30 +0100

    🎉initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-slate-800 min-h-screen text-white flex items-center justify-center">
+        <div className="flex gap-4 border border-white p-4">
+          <div className="bg-red-500 px-4 py-2">test 1</div>
+          <div className="bg-blue-500 px-4 py-2">test 2</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
